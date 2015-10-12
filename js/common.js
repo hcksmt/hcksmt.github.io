4e1e624b57c46a54028b36202a2503ff8e330092
Amos Haviv <mail@amoshaviv.com>
2015-10-12 18:02:28 +0200

    Fixing form values

--- js/common.js
+++ js/common.js
@@ -1,12 +1,35 @@
 var toggleModal = function(headerTxt, msgTxt) {
 	$('#modal-container').fadeToggle('fast');
-	$('#modal-header').text(headerTxt);
 	$('#modal-text').text(msgTxt);
 };
 
+var disableForm = function() {
+	$('#email-input').prop('disabled', true);
+	$('#submit-btn').prop('disabled', true);
+}
+
+var enableForm = function() {
+	$('#email-input').prop('disabled', false);
+	$('#submit-btn').prop('disabled', false);
+}
+
+var validateEmail = function() {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test($('#email-input').val());
+}
+
+
 var subscribe = function() {
-	// $('#email-form').hide();
-	$('#modal-header').text('');
-	$('#modal-text').text('');
+	$('#modal-text').removeClass('error').text('subscribing...');	
+	disableForm();
+
+	if(validateEmail()) {
+		$('#modal-text').text('thank you for subscribing');	
+		$('#submit-btn').text('subscribed!').addClass('success');
+	} else {
+		$('#modal-text').addClass('error').text('Please enter a valid email address');	
+		enableForm();
+	}
+
 	return false;
 };
