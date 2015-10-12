d23417d6c7dc50a1948da155b99ffd39f3ebdeb7
Amos Haviv <mail@amoshaviv.com>
2015-10-12 18:08:42 +0200

    Fixing form values

--- js/common.js
+++ js/common.js
@@ -1,28 +1,36 @@
 var toggleModal = function(headerTxt, msgTxt) {
 	$('#modal-container').fadeToggle('fast');
 
-	$('#email-input').val('');
-	$('#modal-text').text(msgTxt);
+	clearForm();
 	enableForm();
+	
+	$('#modal-text').text(msgTxt);
+};
+
+var clearForm = function() {
+	$('#modal-text').text();
+	$('#submit-btn').removeClass('success');
+	$('#email-input').val('');
 };
 
 var disableForm = function() {
 	$('#email-input').prop('disabled', true);
 	$('#submit-btn').prop('disabled', true);
-}
+};
 
 var enableForm = function() {
 	$('#email-input').prop('disabled', false);
 	$('#submit-btn').prop('disabled', false);
-}
+};
 
 var validateEmail = function() {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test($('#email-input').val());
-}
+};
 
 
 var subscribe = function() {
+
 	$('#modal-text').removeClass('error').text('subscribing...');	
 	disableForm();
 
