d38b175cbe76d09dbbabacf99bcd66f0d25d95be
Amos Haviv <mail@amoshaviv.com>
2015-10-13 13:05:33 +0200

    adding mailchimp

--- js/common.js
+++ js/common.js
@@ -1,26 +1,30 @@
-var toggleModal = function(headerTxt, msgTxt) {
-	$('#modal-container').fadeToggle('fast');
+var reportEvent = function(category, action) {
+    ga('send', 'event', category, action);
+};
 
-	clearForm();
-	enableForm();
-	
-	$('#modal-text').text(msgTxt);
+var toggleModal = function(headerTxt, msgTxt) {
+    $('#modal-container').fadeToggle('fast');
+
+    clearForm();
+    enableForm();
+
+    $('#modal-text').text(msgTxt);
 };
 
 var clearForm = function() {
-	$('#modal-text').text('').removeClass('error');
-	$('#submit-btn').text('subscribe').removeClass('success');
-	$('#email-input').val('');
+    $('#modal-text').text('').removeClass('error');
+    $('#submit-btn').text('subscribe').removeClass('success');
+    $('#email-input').val('');
 };
 
 var disableForm = function() {
-	$('#email-input').prop('disabled', true);
-	$('#submit-btn').prop('disabled', true);
+    $('#email-input').prop('disabled', true);
+    $('#submit-btn').prop('disabled', true);
 };
 
 var enableForm = function() {
-	$('#email-input').prop('disabled', false);
-	$('#submit-btn').prop('disabled', false);
+    $('#email-input').prop('disabled', false);
+    $('#submit-btn').prop('disabled', false);
 };
 
 var validateEmail = function() {
@@ -29,19 +33,43 @@ var validateEmail = function() {
 };
 
 
-var subscribe = function() {
+var postMailChimp = function(cb) {
+    $.ajax({
+        url: '//hacksmith.us11.list-manage.com/subscribe/post-json?u=d34fa4a644386de0931196c68&id=ec056dd018&c=?',
+        type: 'POST',
+        data: 'EMAIL=' + $('#email-input').val(),
+        dataType: 'jsonp',
+        contentType: "application/json; charset=utf-8",
+        success: cb
+    });
+};
 
-	$('#modal-text').removeClass('error').text('subscribing...');	
-	disableForm();
 
-	if(validateEmail()) {
-		$('#modal-text').text('thank you for subscribing');	
-		$('#submit-btn').text('subscribed!').addClass('success');
-	} else {
-		$('#modal-text').addClass('error').text('Please enter a valid email address');	
-		enableForm();
-		$('#email-input').focus();
-	}
+var subscribe = function(e) {
+    reportEvent('subscription', 'clicked');
 
-	return false;
+    $('#modal-text').removeClass('error').text('subscribing...');
+    disableForm();
+
+    if (validateEmail()) {
+        postMailChimp(function(res) {
+        	if (res['result'] != "success") {
+        		reportEvent('subscription', 'error');
+		        $('#modal-text').addClass('error').html(res['msg']);
+		        enableForm();		
+		        $('#email-input').focus();
+        	} else {
+	            reportEvent('subscription', 'success');
+	            $('#modal-text').text('Thank you for joining! Check your email for further instructions!');
+	            $('#submit-btn').text('subscribed!').addClass('success');
+            }
+        });
+    } else {
+        reportEvent('subscription', 'notvalid');
+        $('#modal-text').addClass('error').text('Please enter a valid email address');
+        enableForm();
+        $('#email-input').focus();
+    }
+
+    return false;
 };
